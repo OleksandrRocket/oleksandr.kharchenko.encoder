12c1c783f46d04e7a4550dbcb8818d839d7d1c70
Oleksandr Kharchenko <khalva81@gmail.com>
2023-08-22 15:04:31 +0300

    move  hi

--- src/main/java/ua/javarush/encoder/Application.java
+++ src/main/java/ua/javarush/encoder/Application.java
@@ -4,6 +4,8 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("HI");
-        System.out.println("HI");
-    }
+
+
+
+        System.out.println("HI");}
 }
